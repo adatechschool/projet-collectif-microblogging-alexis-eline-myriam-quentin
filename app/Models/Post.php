1a3ea1b99929ef5f9bac8baca99275e193c406ce
elinelgp <elinelgp@gmail.com>
2023-12-06 15:45:26 +0100

    changements colonnes table posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_id',
+        'user_id',
+        "title",
+        'content',
+        'slug',
+        'featured_image',
+        'is_featured',
+        'is_published'
+    ];
 }
