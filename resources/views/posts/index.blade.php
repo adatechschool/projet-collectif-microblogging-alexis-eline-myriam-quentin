accc7119f66fcd700656e4ab26a93efc30c1ede4
quentinkerzerho <quentin.kerzerho@gmail.com>
2023-12-11 16:49:00 +0100

    Ajout des tests pour les postes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,11 @@
 {{-- Création d'une boucle dans la table post pour afficher le titre, le user_id et le nom de l'utilisateurice
 La syntaxe '-> User -> name' permet de récupérer le nom du user grâce à la relation entre les tables posts et users --}}
-
 <ul>
     @foreach ($posts as $post)
+    <li>{{$post -> User -> name}}</li>
     <li>{{$post -> title}}</li>
     <li><img src={{$post -> featured_image}}></li>
     <li>{{$post -> user_id}}</li>   
-    <li>{{$post -> User -> name}}</li>
     @endforeach
     
 </ul>
--- tests/Feature/Post/TextNotEmptyTest.php
+++ tests/Feature/Post/TextNotEmptyTest.php
@@ -1,20 +1,46 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class TextNotEmptyTest extends TestCase {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_that_true_is_true(): void {
-        $post = Post::factory()->create();
-        $response = $this->get('/posts/' . $post->id);
+        $response = $this->get('/posts');
         $response->assertStatus(200);
         $this->assertTrue(true);
     }
+    public function test_page_return_200() {
+        $url = 'http://127.0.0.1:8001/posts';
+        $headers = get_headers($url,1);
+        $this->assertEquals('HTTP/1.1 200 OK', $headers[0]);
+    }
+    public function test_is_title_displayed(): void {
+        $post = Post::factory()->create();
+        $title = $post->title;
+        $expectedText = sprintf('%s', $title);
+        $response = $this->get('/posts');
+        $response->assertSee($expectedText);        
+    }
+    public function test_is_username_displayed(): void {
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
+        $name = $user -> name;
+        $expectedText = sprintf('%s', $name);
+        $response = $this->get('/posts');
+        $response->assertSee($expectedText);        
+    }
+    
+
 }
