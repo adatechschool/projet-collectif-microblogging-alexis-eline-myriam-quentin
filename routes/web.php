8c4d6e60e0cd767baeb71b3f758244cd172b655b
Myriam Le Nahelec <Myriam1@MacBook-Pro-de-Myriam.local>
2023-12-13 15:37:07 +0100

    view posts

--- routes/web.php
+++ routes/web.php
@@ -33,25 +33,25 @@ Route::middleware('auth')->group(function () {
 });
 //création de la route pour afficher la page /posts, le contenu s'affiche grâce à la fonction getAllPosts
 Route::middleware('auth')->group(function () {
-Route::get('/posts', [PostController::class, 'getAllPosts']);
-// Création de la route pour afficher la page /posts/{id}, le contenu s'affiche grâce à la fonction getOnePost
+    Route::get('/posts', [PostController::class, 'getAllPosts']);
+    // Création de la route pour afficher la page /posts/{id}, le contenu s'affiche grâce à la fonction getOnePost
 
-Route::get('/posts/{id}', [PostController::class, 'getOnePost']);
-// Création de la route pour afficher la page /posts/createPost, le contenu s'affiche grâce à la fonction createPost
-Route::post('/posts/newpost', [PostController::class, 'store']);
-Route::get('/posts/newpost', [PostController::class, 'createPost']);
+    Route::get('/posts/{id}', [PostController::class, 'getOnePost']);
+    // Création de la route pour afficher la page /posts/createPost, le contenu s'affiche grâce à la fonction createPost
+    Route::post('/posts/newpost', [PostController::class, 'store']);
+    Route::get('/posts/newpost', [PostController::class, 'createPost']);
 
 
-Route::get('/posts/{id}', [PostController::class, 'getOnePost']);
-Route::get('/dashboard', [PostController::class, 'getPostByUser'])->name('dashboard');
+    Route::get('/posts/{id}', [PostController::class, 'getOnePost']);
+    Route::get('/dashboard', [PostController::class, 'getPostByUser'])->name('dashboard');
 });
 
 Route::middleware('auth')->group(function () {
 
-//création de la route pour afficher la page /posts, le contenu s'affiche grâce à la fonction getAllPosts
-Route::get('/users', [UserController::class, 'getAllUsers']);
-// Création de la route pour afficher la page /posts/{id}, le contenu s'affiche grâce à la fonction getOnePost
-Route::get('/users/{id}', [UserController::class, 'getOneUser']);
+    //création de la route pour afficher la page /posts, le contenu s'affiche grâce à la fonction getAllPosts
+    Route::get('/users', [UserController::class, 'getAllUsers']);
+    // Création de la route pour afficher la page /posts/{id}, le contenu s'affiche grâce à la fonction getOnePost
+    Route::get('/users/{id}', [UserController::class, 'getOneUser']);
 });
 
 
